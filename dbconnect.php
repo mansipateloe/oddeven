93804b8b70f95b0da218cb01caad278b7992381d
Mansi Patel <maniyamansioe@gmail.com>
2026-07-20 14:00:44 +0530

    do

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,8 @@
-<?php 
-    $conn = mysqli_connect("localhost", "stagi408", "W84PBjTBY18Ap", "stagi408");
-    if($conn === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
- ?>
+<?php
+$conn = mysqli_connect("localhost", "oddev213", "QSVtIhci4d989", "oddev213");
+
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$today_date = date('Y-m-d'); //get today date
