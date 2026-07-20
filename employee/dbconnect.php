5d9cda30395472f5b1347c0e697c529bc8d709ae
Mansi Patel <maniyamansioe@gmail.com>
2026-07-20 13:29:23 +0530

    databse

--- employee/dbconnect.php
+++ employee/dbconnect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = mysqli_connect("localhost", "root", "", "oecrm");
+    $conn = mysqli_connect("localhost", "stagi408", "W84PBjTBY18Ap", "stagi408");
     if($conn === false){
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
