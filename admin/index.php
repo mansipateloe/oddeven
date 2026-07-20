1b9b3186f2587d479d5bdac5122cd8873d220025
Mansi Patel <mansipateloe@gmail.com>
2026-07-20 13:44:59 +0530

    set file

--- admin/index.php
+++ admin/index.php
@@ -1,33 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Oddeven Infotech Pvt. Ltd.</title>
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../vendor/custom/customAdmin.css?v=20260627-3" rel="stylesheet">
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-    <?php include 'dbconnect.php'; ?>
-    <?php require_once __DIR__ . '/../security.php'; ?>
-    <?php require_once __DIR__ . '/../foundation.php'; ?>
-    <?php
+﻿<?php
+include 'dbconnect.php';
+require_once __DIR__ . '/../security.php';
+require_once __DIR__ . '/../foundation.php';
         if (isset($_SESSION['adminId'])) {
             header("Location:dashboard.php");
             exit;
@@ -101,7 +75,35 @@
                                 </div></div>";
             }
         }
-    ?>
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Oddeven Infotech Pvt. Ltd.</title>
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/custom/customAdmin.css?v=20260627-3" rel="stylesheet">
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    
 
     <div class="container">
         <div class="row">
@@ -164,4 +166,5 @@
         window.history.replaceState( null, null, window.location.href );
     }
 </script>
+
 
--- employee/index.php
+++ employee/index.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Oddeven Infotech Pvt. Ltd. || Login</title>
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="../vendor/custom/custom.css?v=20260627-3" rel="stylesheet">
-</head>
-<body style="background: linear-gradient(#edfaff,#bbe8fc);">
-    <?php include 'dbconnect.php'; ?>
-    <?php require_once __DIR__ . '/../security.php'; ?>
-    <?php require_once __DIR__ . '/../foundation.php'; ?>
-    <?php
+﻿<?php
+include 'dbconnect.php';
+require_once __DIR__ . '/../security.php';
+require_once __DIR__ . '/../foundation.php';
         if (isset($_SESSION['employeeId'])) {
             header("Location:home.php");
             exit;
@@ -91,7 +75,25 @@
                                 </div></div>";
             }
         }
-    ?>
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Oddeven Infotech Pvt. Ltd. || Login</title>
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/custom/custom.css?v=20260627-3" rel="stylesheet">
+</head>
+<body style="background: linear-gradient(#edfaff,#bbe8fc);">
+    
     <div class="wrapper">
 <section class="login-form">
     <div class="container">
@@ -157,4 +159,5 @@
         window.history.replaceState( null, null, window.location.href );
     }
 </script>
+
 
