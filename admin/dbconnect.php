28fa2cd5dc3b79494ccd3c88e347af6b0811c00c
Darshan <niravoddevenphp@gmail.com>
2026-09-22 11:32:31 +0530

    Ignore local database connection files

--- admin/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "oecrm");
-    if($conn === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
-    
-    $today_date = date('Y-m-d'); //get today date
- ?>
--- employee/dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $conn = mysqli_connect("localhost", "root", "", "oecrm");
-    if($conn === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
- ?>
